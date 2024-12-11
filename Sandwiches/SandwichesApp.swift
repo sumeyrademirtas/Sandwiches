1a038cccb38cb1893700b346166c0d5ffa3b4e21
Sumeyra Demirtas <smyrdmrts@gmail.com>
2024-12-11 10:35:08 +0300

    Initial Commit

---
+++ Sandwiches/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Sandwiches
+//
+//  Created by Sümeyra Demirtaş on 12/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
